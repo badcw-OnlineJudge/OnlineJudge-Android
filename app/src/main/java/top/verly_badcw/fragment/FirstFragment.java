cbfbcbb8566610e7a8cba12336dc31a3412ba00d
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2018-12-25 13:23:29 +0800

    The firstFragment means to load the problemlist(unfinished)

--- app/src/main/java/top/verly_badcw/fragment/FirstFragment.java
+++ app/src/main/java/top/verly_badcw/fragment/FirstFragment.java
@@ -7,42 +7,88 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import top.verly_badcw.activities.MainActivity;
+import top.verly_badcw.activities.TestActivity;
+import top.verly_badcw.adapter.ListViewDemoAdapter;
 import top.verly_badcw.androidoj.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements View.OnClickListener {
 
-    private Button button;
+    private ListView testLv;//ListView组件
+    private Button updateDataBtn;//动态加载数据组件
+
+    private List<String> dataList = new ArrayList<>();//存储数据
+    private ListViewDemoAdapter listViewDemoAdapter;//ListView的数据适配器
 
     public FirstFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
-        button = view.findViewById(R.id.b2);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity.fragmentManager
-                        .beginTransaction()
-                        .replace(R.id.container,
-                                new SecondFragment(), null)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-
+        View view = inflater.inflate(R.layout.select_layout, container, false);
+        initView(view);//初始化组件
+        initData();//初始化数据
         return view;
+    }
+
+
+    /**
+     * 初始化组件
+     */
+    private void initView(View view) {
+        testLv = (ListView) view.findViewById(R.id.test_lv);
+        updateDataBtn = (Button) view.findViewById(R.id.update_data_btn);
+
+        updateDataBtn.setOnClickListener(this);
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+        //初始化10项数据
+        for (int i = 1; i <= 20; i++) {
+            dataList.add("显示内容" + i);
+        }
+
+        //设置ListView的适配器
+        listViewDemoAdapter = new ListViewDemoAdapter(getActivity(), dataList);
+        testLv.setAdapter(listViewDemoAdapter);
+        testLv.setSelection(4);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.update_data_btn://动态加载列表数据
+                dataList.add("动态加载的数据项");
+                //通知ListView更改数据源
+                if (listViewDemoAdapter != null) {
+                    listViewDemoAdapter.notifyDataSetChanged();
+                    testLv.setSelection(dataList.size() - 1);//设置显示列表的最后一项
+                } else {
+                    listViewDemoAdapter = new ListViewDemoAdapter(getActivity(), dataList);
+                    testLv.setAdapter(listViewDemoAdapter);
+                    testLv.setSelection(dataList.size() - 1);
+                }
+                break;
+        }
     }
 
 }
