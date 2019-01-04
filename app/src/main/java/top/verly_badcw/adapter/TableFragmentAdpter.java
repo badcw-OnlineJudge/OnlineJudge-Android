a933c522e92e58a4378a5356992a23fac04adfac
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-01-04 17:30:15 +0800

    add table view

---
+++ app/src/main/java/top/verly_badcw/adapter/TableFragmentAdpter.java
@@ -0,0 +1,43 @@
+package top.verly_badcw.adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+import top.verly_badcw.activities.MainActivity;
+import top.verly_badcw.fragment.ProblemListFragment;
+import top.verly_badcw.fragment.UserInfoFragment;
+
+public class TableFragmentAdpter extends FragmentPagerAdapter {
+
+    private List<String> list;
+
+    public TableFragmentAdpter(FragmentManager fm, List<String> list) {
+        super(fm);
+        this.list = list;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+
+        // user info
+        if (i == 0) return UserInfoFragment.newInstance(MainActivity.user);
+
+        // problemlist
+        return ProblemListFragment.newInstance(list.get(i));
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return list.get(position);
+    }
+}
