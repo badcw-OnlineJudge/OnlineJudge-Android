9c10542eb2f2bc1b4a56303630f40bf54576316c
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-01-04 17:33:42 +0800

    change the main_activity xml and fragments

--- app/src/main/java/top/verly_badcw/activities/MainActivity.java
+++ app/src/main/java/top/verly_badcw/activities/MainActivity.java
@@ -1,52 +1,87 @@
 package top.verly_badcw.activities;
 
-import android.content.SharedPreferences;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import top.verly_badcw.adapter.TableFragmentAdpter;
 import top.verly_badcw.androidoj.R;
-import top.verly_badcw.fragment.LoginFragment;
+import top.verly_badcw.data.Languages;
+import top.verly_badcw.data.Messages;
+import top.verly_badcw.util.User;
 
-public class MainActivity extends AppCompatActivity implements LoginFragment.OnMessageReadListener {
+public class MainActivity extends AppCompatActivity {
 
-    public static final int SUCCESS = 1;
-    public static final int FAIL = 0;
-    public static final int CONNECT_WRONG = 2;
-    public static final int PASSWORD_WRONG = 3;
-    public static final int ACCESS_WRONG = 4;
+    public static MainActivity instance;
+    public static User user;
 
-    public static FragmentManager fragmentManager;
-    private TextView txt;
-
-
-    public static SharedPreferences preferences;
-    public static SharedPreferences.Editor editor;
-
-
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getSupportFragmentManager();
+        instance = this;
 
-        preferences = getSharedPreferences("verly-badcw", MODE_PRIVATE);
-        editor = preferences.edit();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabLayout tableLayout = (TabLayout) findViewById(R.id.table_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, 0, 0);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        FragmentTransaction f = fragmentManager.beginTransaction();
+        initUser();
 
-        LoginFragment secondFragment = new LoginFragment();
-        f.add(R.id.container, secondFragment, null);
-        f.commit();
+        List<Integer> p = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            p.add(i);
+        }
+        user.setSolvedProblems(p);
 
-        txt = findViewById(R.id.txt_display_message);
+        List<String> list = new ArrayList<>();
+        list.add(Messages.get(this, "userinfo"));
+        list.add(Messages.get(this, "problemlist"));
+
+        viewPager.setAdapter(new TableFragmentAdpter(getSupportFragmentManager(), list));
+        tableLayout.setupWithViewPager(viewPager);
     }
 
-    @Override
-    public void onMessageRead(String message) {
-        txt.setText(message);
+    private static void initUser() {
+        user = new User();
+        user.setUserName("TEST");
+        user.setRank(1);
+    }
+
+    public static void language(Languages lang) {
+        Preference.INSTANCE.put( Preference.KEY_LANG, lang.code());
+    }
+
+    public static Languages language() {
+        String code = Preference.INSTANCE.getString(Preference.KEY_LANG, null);
+        if (code == null){
+            Languages lang = Languages.matchLocale(Locale.getDefault());
+            return lang;
+        }
+        else return Languages.matchCode(code);
+    }
+
+    public static void reportException( Throwable tr ) {
+        Log.e("ACTIVITY", Log.getStackTraceString(tr));
     }
 }
