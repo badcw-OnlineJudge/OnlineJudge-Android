3da89b3c8474f4d0576a56d43896b49bf6133670
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-01-05 17:17:23 +0800

    change hostname to public

--- app/src/main/java/top/verly_badcw/data/APIUrl.java
+++ app/src/main/java/top/verly_badcw/data/APIUrl.java
@@ -1,7 +1,7 @@
 package top.verly_badcw.data;
 
 public class APIUrl {
-    private final static String HOST_NAME = "http://verly-badcw.top:8020";
+    public final static String HOST_NAME = "http://verly-badcw.top:8020";
     public final static String LOGIN_URL = HOST_NAME + "/api/login";
     public final static String PROFILE_URL = HOST_NAME + "/api/profile";
 }
