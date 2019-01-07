0e288e4285ce354f05e6e1b5f84872d11dcb554f
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-01-07 21:09:26 +0800

    add status info in javaResources(rename from URLapi)

---
+++ app/src/main/java/top/verly_badcw/InJavaResources.java
@@ -0,0 +1,19 @@
+package top.verly_badcw;
+
+public class InJavaResources {
+    public final static String HOST_NAME = "http://verly-badcw.top:8020";
+    public final static String LOGIN_URL = HOST_NAME + "/api/login";
+    public final static String PROFILE_URL = HOST_NAME + "/api/profile";
+    public final static int INTNULL = -100;
+    public final static int COMPILE_ERROR_STATUS = -2;
+    public final static int WRONG_ANSWER_STATUS = -1;
+    public final static int ACCEPTED_STATUS = 0;
+    public final static int CPU_TIME_LIMIT_EXCEEDED_STATUS = 1;
+    public final static int REAL_TIME_LIMIT_EXCEEDED_STATUS = 2;
+    public final static int MEMORY_LIMIT_EXCEEDED_STATUS = 3;
+    public final static int RUNTIME_ERROR_STATUS = 4;
+    public final static int SYSTEM_ERROR_STATUS = 5;
+    public final static int PENDING_STATUS = 6;
+    public final static int JUDGING_STATUS = 7;
+    public final static int PARTIALLY_ACCEPTED_STATUS = 8;
+}
--- app/src/main/java/top/verly_badcw/data/APIUrl.java
+++
@@ -1,7 +0,0 @@
-package top.verly_badcw.data;
-
-public class APIUrl {
-    public final static String HOST_NAME = "http://verly-badcw.top:8020";
-    public final static String LOGIN_URL = HOST_NAME + "/api/login";
-    public final static String PROFILE_URL = HOST_NAME + "/api/profile";
-}
